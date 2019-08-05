0e943bb6916d5297ef6d84f36f0cc381e9e4f653
Marlon Burnett <marlon.morato@iteris.com.br>
2019-08-05 16:17:12 -0300

    sample code

---
+++ Dark ModeTests/Dark_ModeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dark_ModeTests.swift
+//  Dark ModeTests
+//
+//  Created by Marlon Morato on 26/06/19.
+//  Copyright © 2019 Marlon Morato. All rights reserved.
+//
+
+import XCTest
+@testable import Dark_Mode
+
+class Dark_ModeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# demo-ios-dark-mode
+# iOS Dark Mode - DEMO
 iOS now lives in the Dark!
+
+Este repositório contém um projeto de exemplo que ilustra o artigo hands-on sobre o Dark Mode no iOS publicado no [blog da Iteris](http://blog.iteris.com.br). 
+
+Você pode acessar este artigo a partir do link [http://blog.iteris.com.br](http://blog.iteris.com.br).
